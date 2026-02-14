f65af463f033c2a50409feb0179d9b11bed0dab3
sketchgamecode <101815141+sketchgamecode@users.noreply.github.com>
2026-02-14 14:52:57 +0800

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+        base: '/SWorld/',  // 👈 添加这一行
       server: {
         port: 3000,
         host: '0.0.0.0',
